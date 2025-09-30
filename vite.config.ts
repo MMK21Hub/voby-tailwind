86c314a38386065189bd3939b67e07949b1099aa
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-30 08:37:43 +0100

    Migrate to Tailwind 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import voby from "voby-vite";
+import { defineConfig } from "vite"
+import voby from "voby-vite"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [voby()],
-});
+  plugins: [voby(), tailwindcss()],
+})
